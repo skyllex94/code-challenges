aff21cd2078dce470d64764a39f5db641cccc16a
Kamen Kanchev <45595147+skyllex94@users.noreply.github.com>
2023-01-11 22:29:53 +0200

    dynamic programming to solve permutation on climbing steps

--- 70. climbStars.js
+++ 70. climbStars.js
@@ -14,20 +14,6 @@ function newPath(step, limit, ways) {
     newPath(step + 1, limit, ways) + newPath(step + 2, limit, ways);
 }
 
-// First working approach hitting runtime issues with scaling
-var climbStairs = function (n) {
-  var start = 0;
-  var result = newPath(start, n);
-  console.log("result:", result);
-};
-
-function newPath(step, limit) {
-  if (step > limit) return 0;
-  if (step === limit) return 1;
-
-  return newPath(step + 1, limit) + newPath(step + 2, limit);
-}
-
 // Second approach wt initial recurssion - hitting runtime issues
 var climbStairs2 = function (limit, step = 0) {
   if (step > limit) return 0;
@@ -38,4 +24,44 @@ var climbStairs2 = function (limit, step = 0) {
   return result;
 };
 
-climbStairs0(10);
+// Third approach - working - Runtime >64%, Memory >76%
+var climbStairs = function (n) {
+  var start = 0;
+  var memo = [];
+  var result = newPath(start, n, memo);
+  console.log("result:", result);
+  return result;
+};
+
+function newPath(step, limit, memo) {
+  if (step > limit) return 0;
+  if (step === limit) return 1;
+  if (memo[step]) return memo[step];
+
+  memo[step] = newPath(step + 1, limit, memo) + newPath(step + 2, limit, memo);
+  // You can add additional steps as you please - newPath(step + 3, limit, memo);
+
+  return memo[step];
+}
+
+// Forth Approach - Runtime >94%, Memory >7%
+var climbStairs4 = function (n) {
+  var sum = 0,
+    first = 0,
+    second = 0;
+
+  for (var i = 0; i < n; i++) {
+    if (i === 0) {
+      first = i;
+      second = i + 1;
+    }
+
+    sum = first + second;
+    first = second;
+    second = sum;
+  }
+  console.log("sum:", sum);
+  return sum;
+};
+
+climbStairs(10);
