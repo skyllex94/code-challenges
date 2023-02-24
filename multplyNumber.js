fc854dea6e4b4298e557e5ed485f2fe5b2203d63
Kamen Kanchev <45595147+skyllex94@users.noreply.github.com>
2023-02-24 18:19:09 -0500

    interview questions asked

---
+++ multplyNumber.js
@@ -0,0 +1,23 @@
+function multiplyByTwo(...prop) {
+  let arr = [...prop];
+  return arr.map((curr) => curr * 2);
+}
+
+console.log(multiplyByTwo(1, 2, 3, 4, 5)); // return [2,4,6,8,10]
+
+
+
+const obj= {
+    a: 8,
+    say: function() {
+    // const foo = this.a;
+    console.log(foo);
+      setTimeout(function(this.a) {
+        console.log(this.a);
+      }, 1000);
+    }
+  };
+  
+  
+  obj.say();
+  
